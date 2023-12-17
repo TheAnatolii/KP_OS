59e10a9ed32715a8d2da1a39a78d5f8d3a576a78
AnatoliiG <avgolovenko@mai.education>
2023-12-17 18:32:29 +0300

    update checker

--- checker.cpp
+++ checker.cpp
@@ -5,62 +5,89 @@
 #include <unordered_set>
 #include <queue>
 
-struct Job
+std::unordered_set<int> getStartJobs(const std::unordered_map<std::string, std::unordered_set<std::string>> &graph, bool &hasStartJobs)
 {
-    std::vector<int> dependencies;
-};
-
-std::unordered_set<int> getStartJobs(std::unordered_map<int, Job> &jobs)
-{
-    std::unordered_set<int> allJobs;
-    std::unordered_set<int> dependentJobs;
     std::unordered_set<int> startJobs;
 
-    for (const auto &job : jobs)
+    std::unordered_set<std::string> allNodes;
+    std::unordered_set<std::string> dependentNodes;
+
+    for (const auto &node : graph)
     {
-        allJobs.insert(job.first);
-        for (int dependency : job.second.dependencies)
+        allNodes.insert(node.first);
+        for (const auto &dependency : node.second)
         {
-            dependentJobs.insert(dependency);
+            dependentNodes.insert(dependency);
         }
     }
 
-    for (const auto &job : jobs)
+    for (const auto &node : graph)
     {
-        if (dependentJobs.find(job.first) == dependentJobs.end())
+        // Проверяем, является ли node.first числом перед преобразованием в int
+        if (std::all_of(node.first.begin(), node.first.end(), ::isdigit))
         {
-            startJobs.insert(job.first);
+            int converted = std::stoi(node.first);
+            if (dependentNodes.find(node.first) == dependentNodes.end() && allNodes.find(node.first) != allNodes.end())
+            {
+                startJobs.insert(converted);
+            }
         }
     }
+
+    hasStartJobs = !startJobs.empty(); // Обновление значения hasStartJobs
 
     return startJobs;
 }
 
-std::unordered_set<int> getEndJobs(std::unordered_map<int, Job> &jobs)
-{
-    std::unordered_set<int> allJobs;
-    std::unordered_set<int> dependentJobs;
-    std::unordered_set<int> endJobs;
+// std::unordered_set<int> getEndJobs(const std::unordered_map<std::string, std::unordered_set<std::string>> &graph, bool &hasEndJobs)
+// {
+//     std::unordered_set<int> endJobs;
 
-    for (const auto &job : jobs)
-    {
-        allJobs.insert(job.first);
-        for (int dependency : job.second.dependencies)
-        {
-            dependentJobs.insert(dependency);
-        }
-    }
+//     std::unordered_set<std::string> allNodes;
+//     std::unordered_set<std::string> dependentNodes;
 
-    for (const auto &job : jobs)
-    {
-        if (dependentJobs.find(job.first) == dependentJobs.end())
-        {
-            endJobs.insert(job.first);
-        }
-    }
+//     for (const auto &node : graph)
+//     {
+//         allNodes.insert(node.first);
+//         for (const auto &dependency : node.second)
+//         {
+//             dependentNodes.insert(dependency);
+//         }
+//     }
 
-    return endJobs;
-}
+//     for (const auto &node : graph)
+//     {
+//         for (const auto &neighbor : node.second)
+//         {
+//             dependentNodes.insert(neighbor); // Заполняем dependentNodes зависимостями других узлов
+//             allNodes.insert(node.first);     // Заполняем allNodes текущими узлами
+//         }
+//     }
+
+//     // Отфильтруем endJobs для получения корректных завершающих узлов
+//     for (const auto &node : graph)
+//     {
+//         dependentNodes.erase(node.first);                             // Удаляем текущий узел из dependentNodes
+//         dependentNodes.erase(node.second.begin(), node.second.end()); // Удаляем зависимости текущего узла из dependentNodes
+//     }
+
+//     for (const auto &node : graph)
+//     {
+//         // Проверяем, является ли node.first числом перед преобразованием в int
+//         if (std::all_of(node.first.begin(), node.first.end(), ::isdigit))
+//         {
+//             int converted = std::stoi(node.first);
+//             if (dependentNodes.find(node.first) == dependentNodes.end() && allNodes.find(node.first) != allNodes.end())
+//             {
+//                 endJobs.insert(converted);
+//             }
+//         }
+//     }
+
+//     hasEndJobs = !endJobs.empty(); // Обновление значения hasEndJobs
+
+//     return endJobs;
+// }
 
 // Функция для проверки наличия циклов в DAG с использованием алгоритма поиска в глубину (DFS)
 bool hasCycle(const std::unordered_map<std::string, std::unordered_set<std::string>> &graph,
@@ -92,7 +119,6 @@ bool hasCycle(const std::unordered_map<std::string, std::unordered_set<std::stri
     return false;
 }
 
-// Функция для проверки наличия только одной компоненты связанности
 void DFS(const std::unordered_map<std::string, std::unordered_set<std::string>> &graph,
          const std::string &node,
          std::unordered_set<std::string> &visited)
@@ -101,7 +127,7 @@ void DFS(const std::unordered_map<std::string, std::unordered_set<std::string>> 
     auto it = graph.find(node);
     if (it != graph.end())
     {
-        for (const auto &neighbor : it->second)
+        for (const auto &neighbor : graph.at(node))
         {
             if (visited.find(neighbor) == visited.end())
             {
@@ -114,21 +140,18 @@ void DFS(const std::unordered_map<std::string, std::unordered_set<std::string>> 
 bool hasSingleConnectedComponent(const std::unordered_map<std::string, std::unordered_set<std::string>> &graph)
 {
     if (graph.empty())
-        return true;
-
-    std::unordered_set<std::string> visited;
-    int components = 0;
-
-    for (const auto &node : graph)
     {
-        if (visited.find(node.first) == visited.end())
-        {
-            DFS(graph, node.first, visited);
-            components++;
-        }
+        return true;
     }
 
-    return components == 1;
+    std::unordered_set<std::string> visited;
+
+    // Выбираем первый узел и проверяем связанность
+    const auto &firstNode = graph.begin()->first;
+    DFS(graph, firstNode, visited);
+
+    // Проверяем, что все узлы были посещены (одна компонента связности)
+    return visited.size() == graph.size();
 }
 
 int main()
@@ -139,8 +162,7 @@ int main()
     fin.close();
 
     // Парсинг YAML и построение графа
-    std::unordered_map<int, Job> jobs;
-    int job_id;
+    std::unordered_map<std::string, std::unordered_set<std::string>> graph;
 
     for (const auto &node : config)
     {
@@ -169,8 +191,10 @@ int main()
     bool singleComponent = hasSingleConnectedComponent(graph);
 
     // Проверка наличия стартовых и завершающих джобов
-    std::unordered_set<int> startJobs = getStartJobs(jobs);
-    std::unordered_set<int> endJobs = getEndJobs(jobs);
+    bool hasStartJobs = false;
+    bool hasEndJobs = false;
+    std::unordered_set<int> startJobs = getStartJobs(graph, hasStartJobs);
+    // std::unordered_set<int> endJobs = getEndJobs(graph, hasEndJobs);
 
     // Вывод результатов проверки
     if (hasCycles)
@@ -181,13 +205,18 @@ int main()
     {
         std::cout << "Граф содержит несколько компонент связанности.\n";
     }
-    else if (startJobs.empty() || endJobs.empty())
+    else if (hasStartJobs || hasEndJobs)
     {
-        std::cout << "Отсутствуют стартовые или завершающие джобы!" << std::endl;
+        std::cout << "Отсутствуют ";
+        if (hasStartJobs)
+            std::cout << "стартовые ";
+        if (hasEndJobs)
+            std::cout << "или завершающие ";
+        std::cout << "джобы." << std::endl;
     }
     else
     {
-        std::cout << "Проверка завершена. Граф корректен." << std::endl;
+        std::cout << "Граф корректен.\n";
     }
 
     return 0;
