fa7b89e8ccf77d80163fa033f3d8b12219076c5e
AnatoliiG <avgolovenko@mai.education>
2023-12-29 16:39:24 +0300

    add files

--- checker.cpp
+++ checker.cpp
@@ -9,6 +9,8 @@
 #include <thread>
 #include <mutex>
 #include <string>
+#include <signal.h>
+#include <unistd.h>
 
 // структура для джоба
 struct Job
@@ -51,7 +53,7 @@ std::vector<Job> parser(std::string filename)
 }
 
 // создаём дерево, которое показывает переходы от данной вершины к её родителю(ям)
-std::vector<std::unordered_set<int>> create_tree_from_up_to_down(const std::vector<Job> &jobs, std::vector<int> &start_jobs)
+std::vector<std::unordered_set<int>> create_tree_from_up_to_down(const std::vector<Job> &jobs, std::unordered_set<int> &start_jobs)
 {
     std::vector<std::unordered_set<int>> tree(jobs.size());
 
@@ -59,7 +61,7 @@ std::vector<std::unordered_set<int>> create_tree_from_up_to_down(const std::vect
     {
         if (job.depends_on.empty())
         {
-            start_jobs.push_back(job.name);
+            start_jobs.insert(job.name);
         }
 
         for (auto &dependence : job.depends_on)
@@ -71,7 +73,7 @@ std::vector<std::unordered_set<int>> create_tree_from_up_to_down(const std::vect
 }
 
 // находим конечные джобы
-void find_end_jobs(const std::vector<std::unordered_set<int>> &tree, std::vector<int> &end_jobs)
+void find_end_jobs(const std::vector<std::unordered_set<int>> &tree, std::unordered_set<int> &end_jobs)
 {
     std::vector<bool> is_top(tree.size(), true);
 
@@ -87,7 +89,7 @@ void find_end_jobs(const std::vector<std::unordered_set<int>> &tree, std::vector
     {
         if (is_top[i])
         {
-            end_jobs.push_back(i + 1);
+            end_jobs.insert(i + 1);
         }
     }
 }
@@ -114,18 +116,13 @@ bool find_cycle(std::vector<std::unordered_set<int>> &tree, int u, std::vector<i
 }
 
 // прогоняем функцию нахождения циклов для каждой вершины дерева
-bool check_cycle(std::vector<std::unordered_set<int>> &tree, std::vector<int> &starts)
+bool check_cycle(std::vector<std::unordered_set<int>> &tree, std::unordered_set<int> &starts)
 {
     std::vector<int> visited(tree.size());
     bool res = true;
     for (int start : starts)
     {
         res &= !find_cycle(tree, start - 1, visited);
-        // for (auto &children : visited)
-        // {
-        //     std::cout << children << " ";
-        // }
-        // std::cout << std::endl;
     }
     return res;
 }
@@ -168,17 +165,53 @@ bool find_components(std::vector<std::unordered_set<int>> &tree, int u, std::vec
 }
 
 // проверка графа на одну компоненту связности
-bool check_connectivity(std::vector<std::unordered_set<int>> &tree, std::vector<int> &starts)
+bool check_connectivity(std::vector<std::unordered_set<int>> &tree, std::unordered_set<int> &starts)
 {
     std::vector<int> visited(tree.size());
     bool res = true;
     size_t i = 0;
-    dfs(tree, starts[i++] - 1, visited);
-    for (; i < starts.size(); i++)
+    dfs(tree, *(starts.begin()) - 1, visited);
+    for (auto itr = std::next(starts.begin(), 1); itr != starts.end(); ++itr)
     {
-        res &= !find_components(tree, starts[i] - 1, visited);
+        res &= !find_components(tree, *itr - 1, visited);
     }
     return res;
+}
+
+void killall_jobs(std::queue<std::pair<int, pid_t>> &q)
+{
+    while (!q.empty())
+    {
+        auto [id, pid] = q.front();
+        q.pop();
+        if (pid != -1)
+            kill(pid, SIGINT);
+    }
+}
+
+pid_t create_job(std::string command)
+{
+    std::stringstream ss{command};
+    std::vector<std::string> strargs;
+    std::vector<const char *> args;
+    std::string str;
+    while (ss >> str)
+    {
+        strargs.push_back(str);
+        args.push_back(str.c_str());
+    }
+
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        execv(args[0], const_cast<char *const *>(args.data()));
+        pid = -2;
+        std::cerr << "exec error" << std::endl;
+        exit(-2);
+    }
+
+    std::cout << "create " << pid << ": " << command << std::endl;
+    return pid;
 }
 
 int main()
@@ -186,7 +219,16 @@ int main()
 
     std::vector<Job> jobs = parser("new_test.yaml"); // читаем файл
 
-    std::vector<int> start_jobs, end_jobs;
+    // for (auto &dependence : jobs)
+    // {
+    //     for (auto &node : dependence.depends_on)
+    //     {
+    //         std::cout << node << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    std::unordered_set<int> start_jobs, end_jobs;
     // создаём дерево, и сразу находим начальные job'ы
     std::vector<std::unordered_set<int>> tree = create_tree_from_up_to_down(jobs, start_jobs);
 
@@ -219,7 +261,7 @@ int main()
     std::unordered_map<std::string, bool> mutex_vals;
     std::unordered_map<int, std::string> mutex_names;
 
-    for (auto job : jobs)
+    for (auto &job : jobs)
     {
         std::string mutex_name = job.mutex_name;
         if (!mutex_name.empty())
@@ -229,14 +271,99 @@ int main()
         }
     }
 
-    // for (auto &mutex : mutex_names)
-    // {
-    //     std::cout << mutex.first << " " << mutex.second << std::endl;
-    // }
-
     // начинаем запуск DAG'а
     std::queue<std::pair<int, pid_t>> waitq; // очередь задач
     std::mutex qmtx;                         // мьютекс для доступа к waitq
 
+    std::thread wait_thread([&]()
+                            {
+        while (!end_jobs.empty() || !waitq.empty()) {
+            if (!waitq.empty()) {
+                pid_t pid;
+                int id;
+                {
+                    std::lock_guard<std::mutex> lock(qmtx);
+                    id = waitq.front().first;
+                    pid = waitq.front().second;
+                    waitq.pop();
+                }
+                // проверка нужно ли ждать этот процесс, -1 значит, что команда была запущена с помощью system()
+                if (pid != -1) {
+                    int sig;
+                    // Ловим ошибку с помощью waitpid
+                    std::cout << "wait " << pid << std::endl;
+                    waitpid(pid, &sig, 0);
+                    if (WIFSIGNALED(sig)) {
+                        // если ошибка найдена, то завершаем все процессы и переходим к завершению программы
+                        std::cout << "signal is heared from " << id << std::endl;
+                        std::lock_guard<std::mutex> lock(qmtx);
+                        end_jobs = {};
+                        killall_jobs(waitq);
+                    }
+                }
+                // Разблокируем мьютекс джобы
+                {
+                    std::lock_guard<std::mutex> lock(qmtx);
+                    mutex_vals[mutex_names[id - 1]] = true;
+                }
+                // добавляем в очередь запуска готовые к выполнению джобы
+                for (int i = 0; i < tree.size(); ++i) {
+                    if (tree[i].count(id)) {
+                        tree[i].erase(id);
+                        if (tree[i].empty()) {
+                            std::lock_guard<std::mutex> lock(qmtx);
+                            start_jobs.insert(i + 1);
+                            std::cout << i + 1 << std::endl;
+                        }
+                    }
+                }
+                end_jobs.erase(id);
+            }
+        } });
+
+    while (!end_jobs.empty())
+    {
+        // контейнер для запоминания запущенных джобов, чтобы удалить их из start_points
+        std::vector<int> erase_id;
+        std::lock_guard<std::mutex> lock(qmtx);
+        for (int id : start_jobs)
+        {
+            if ((mutex_names.count(id) && mutex_vals[mutex_names[id]]) || !mutex_names.count(id))
+            {
+                std::string command = jobs[id - 1].command;
+                if (command.front() != '.' && command.front() != '/')
+                {
+                    // если команда не является выполнимым файлом, то запускаем её с помощью system()
+                    int success = std::system(command.c_str());
+                    if (success == -1)
+                    {
+                        killall_jobs(waitq);
+                    }
+                    erase_id.push_back(id);
+                    waitq.push({id, -1});
+                }
+                else
+                {
+                    // иначе делаем fork exec
+                    pid_t pid = create_job(command);
+
+                    // блокируем мьютекс джобы
+                    if (mutex_names.count(id))
+                        mutex_vals[mutex_names[id]] = false;
+
+                    erase_id.push_back(id);
+                    // добавляем процесс в очередь для ожидания
+                    waitq.push({id, pid});
+                }
+            }
+        }
+        for (int id : erase_id)
+        {
+            start_jobs.erase(id);
+        }
+    }
+
+    killall_jobs(waitq);
+    wait_thread.join();
     return 0;
 }
