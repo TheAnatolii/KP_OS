825e95154a43a0e8d1ff0a159849dc2d88e03436
AnatoliiG <avgolovenko@mai.education>
2023-12-29 22:44:28 +0300

    add files

--- build/yaml-cpp-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master
-# Build directory: /Users/anatolii/Desktop/Oper_Syst/kp/build/yaml-cpp-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
--- build/yaml-cpp-master/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/util
-# Build directory: /Users/anatolii/Desktop/Oper_Syst/kp/build/yaml-cpp-master/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
