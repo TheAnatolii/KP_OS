4ac16e8e5ab9e62fc7e842014a51b83804482c60
AnatoliiG <avgolovenko@mai.education>
2023-12-29 16:39:54 +0300

    add files

---
+++ build/yaml-cpp-master/CMakeFiles/yaml-cpp.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/binary.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/binary.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/binary.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/contrib/graphbuilder.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/contrib/graphbuilder.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/contrib/graphbuilder.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/contrib/graphbuilderadapter.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/contrib/graphbuilderadapter.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/contrib/graphbuilderadapter.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/convert.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/convert.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/convert.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/depthguard.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/depthguard.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/depthguard.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/directives.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/directives.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/directives.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/emit.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emit.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emit.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/emitfromevents.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emitfromevents.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emitfromevents.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/emitter.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emitter.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emitter.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/emitterstate.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emitterstate.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emitterstate.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/emitterutils.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emitterutils.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/emitterutils.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/exceptions.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/exceptions.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/exceptions.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/exp.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/exp.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/exp.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/memory.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/memory.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/memory.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/node.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/node.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/node.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/node_data.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/node_data.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/node_data.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/nodebuilder.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/nodebuilder.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/nodebuilder.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/nodeevents.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/nodeevents.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/nodeevents.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/null.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/null.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/null.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/ostream_wrapper.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/ostream_wrapper.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/ostream_wrapper.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/parse.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/parse.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/parse.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/parser.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/parser.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/parser.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/regex_yaml.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/regex_yaml.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/regex_yaml.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/scanner.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/scanner.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/scanner.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/scanscalar.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/scanscalar.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/scanscalar.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/scantag.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/scantag.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/scantag.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/scantoken.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/scantoken.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/scantoken.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/simplekey.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/simplekey.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/simplekey.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/singledocparser.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/singledocparser.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/singledocparser.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/stream.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/stream.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/stream.cpp.o.d"
+  "/Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/src/tag.cpp" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/tag.cpp.o" "gcc" "yaml-cpp-master/CMakeFiles/yaml-cpp.dir/src/tag.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/yaml-cpp-master/util/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/anatolii/Desktop/Oper_Syst/kp/yaml-cpp-master/util
+# Build directory: /Users/anatolii/Desktop/Oper_Syst/kp/build/yaml-cpp-master/util
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
